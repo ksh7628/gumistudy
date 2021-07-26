b1aaba7f0fd64e00190bfb6ba00b6dbf46c961af
starempty <40350280+starempty@users.noreply.github.com>
2021-07-26 17:11:28 +0900

    Update and rename Main_bj_17140_이차원배열과연산.java to Main_bj_17140_이차원배열과연산_김소빈.java

--- Algorithm/21년/7월/3주차/Main_bj_17140_이차원배열과연산_김소빈.java
+++ Algorithm/21년/7월/3주차/Main_bj_17140_이차원배열과연산_김소빈.java
@@ -1,5 +1,3 @@
-package a0722;
-
 import java.util.*;
 
 public class Main_bj_17140_이차원배열과연산 {
@@ -62,11 +60,6 @@ public class Main_bj_17140_이차원배열과연산 {
 					}
 				}
 			}
-//			System.out.println(row+" "+col);
-//			for(int i = 0; i < 6; i++) {
-//				for(int j = 0; j < 6; j++) System.out.print(arr[i][j]+" ");
-//				System.out.println();
-//			}
 			cnt++;
 		}
 		System.out.println(cnt>100?-1:cnt);
@@ -96,32 +89,5 @@ public class Main_bj_17140_이차원배열과연산 {
 			tmp[idx++] = cur.time;
 		}
 		return tmp;
-	}
-	static void sorting(int[][]arr, boolean type) {
-		int size = 0;
-		for(int i = 0; i < row; i++) {
-			Map<Integer, Integer> mapper = new HashMap<>();
-			for(int j = 0; j < col; j++) {
-				int cur = arr[i][j];
-				if(cur == 0) continue;
-				arr[i][j] = 0;
-				if(mapper.containsKey(cur)) mapper.put(cur, mapper.get(cur)+1);
-				else mapper.put(cur, 1);
-			}
-			size = Integer.max(size, mapper.size()*2);
-			List<pair> li = new ArrayList<>();
-			for(int key: mapper.keySet()) {
-				int time = mapper.get(key);
-				li.add(new pair(key, time));
-			}
-			Collections.sort(li);
-			int idx = 0;
-			for(pair cur: li) {
-				arr[i][idx++] = cur.key;
-				arr[i][idx++] = cur.time;
-			}
-		}
-		if(type) col = size;
-		else row = size;
 	}
 }
